864301382e1476d071952228792d9ecdcf0266eb
souledgex <54240246+reyhaonan@users.noreply.github.com>
2020-11-19 20:32:35 +0700

    Preview to mobile
    the development server now can be accessed on mobile by entering {computer_IP:3000} in the browser

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,9 +3,53 @@
     <Nuxt />
   </div>
 </template>
+
+
 <style lang="scss">
+//used as library
+
+@import url('https://fonts.googleapis.com/css2?family=Boogaloo&family=Varela+Round&display=swap');
+//Varela round for general use, whilst Boogaloo used for app title cards
+
+$red: #EF476F;
+$yellow: #FFD166;
+$green: #06D6A0;
+$blue: #118AB2;
+$darkblue: #073B4C;
+
 *{
   box-sizing: border-box;
   margin: 0;padding: 0;
+  font-family: 'Varela Round', sans-serif;
+}
+
+.font-boogaloo{
+  font-family: 'Boogaloo', cursive;
+}
+
+//standard black color for the text
+.font-black{
+  color: #434343;
+}
+
+//standard white color for the text
+.font-white{
+  color: #f8f8f8;
+}
+
+.bg-red{
+  background-color: #EF476F;
+}
+.bg-yellow{
+  background-color: #FFD166;
+}
+.bg-green{
+  background-color: #06D6A0;
+}
+.bg-blue{
+  background-color: #118AB2;
+}
+.bg-darkblue{
+  background-color: #073B4C;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,6 @@
 <template>
-  <div class="container">
-    <div class="flex">
-      <div id="illustration">
-        Left
-      </div>
-      <div id="form">
-        <NuxtLink to="/login_guru"> Masuk Sebagai Guru </NuxtLink>
-        <NuxtLink to="/login_murid"> Masuk Sebagai Murid </NuxtLink>
-        <a href="" class="test">asd</a>
-      </div>
-    </div>
+  <div>
+    <div class="apptitle font-boogaloo">DIAJARS</div>
   </div>
 </template>
 
@@ -18,5 +9,7 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
-
+.apptitle{
+  font-size: 90px;
+}
 </style>
