86ebf62b88027e2cc7a780daabab0266728982f1
souledgex <54240246+reyhaonan@users.noreply.github.com>
2020-11-23 19:49:55 +0700

    Create beranda_guru.vue

---
+++ pages/beranda_guru.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container bg-red">
+    <div class="taskbar">
+
+    </div>
+
+    <div class="whitecard">
+
+    </div>
+
+    <i class="material-icons">face</i>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .container{
+    // display: flex;
+    height: 100%;
+  }
+  .taskbar{
+    position: absolute;
+    z-index: 1;
+  }
+  .whitecard{
+    height: 100%;
+    width: 100%;
+    z-index: 2;
+    background: white;
+  }
+
+</style>
