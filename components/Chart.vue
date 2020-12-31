58f0ca2a7b1df377f502b869dc9ad0359fa619a6
souledgex <54240246+reyhaonan@users.noreply.github.com>
2020-12-31 12:02:13 +0700

    chart

---
+++ components/Chart.vue
@@ -0,0 +1,46 @@
+<script>
+import { Bar } from 'vue-chartjs'
+
+export default {
+  extends: Bar,
+  props: ["chartData"],
+  mounted () {
+    this.renderChart({
+      labels: ['Nilai Agama', 'Motorik halus','Motorik kasar','Bahasa','Kognitif','Sosial emosi','Seni'],
+      datasets: [
+        {
+          label: 'Skor perkembangan anak',
+          backgroundColor: [
+                'rgba(255, 99, 132)',
+                'rgba(54, 162, 235)',
+                'rgba(255, 206, 86)',
+                'rgba(75, 192, 192)',
+                'rgba(153, 102, 255)',
+                'rgba(255, 159, 64)',
+                'rgb(70,189,135)'
+            ],
+          data: [this.chartData.nilaiAgama,
+                this.chartData.motorikHalus,
+                this.chartData.motorikKasar,
+                this.chartData.bahasa,
+                this.chartData.bahasa,
+                this.chartData.kognitif,
+                this.chartData.sosialEmosi,
+                this.chartData.seni
+                ]
+        },
+      ]
+    },
+    {
+      responsive: false,
+      scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    })
+  }
+}
+</script>
