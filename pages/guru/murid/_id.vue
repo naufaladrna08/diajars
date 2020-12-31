b569ba47e4ac481b4de43c4d9219d83bdc3777f1
souledgex <54240246+reyhaonan@users.noreply.github.com>
2020-12-31 12:03:49 +0700

    Update _id.vue

--- pages/guru/murid/_id.vue
+++ pages/guru/murid/_id.vue
@@ -4,19 +4,46 @@
     <div class="infoSiswa">
       <Button bg="white" class="back_button" v-on:buttonClick="() => $router.push('/guru/murid')" :noShadow="true"><i class="material-icons">chevron_left</i></Button>
     </div>
-    <div class="chartContainer">
+    <div class="whiteBox">
       Perkembangan siswa
-      <div class="chart"></div>
+      <div class="chartContainer">
+        <Chart style="height: 100%;width:100% !important" :chartData="chartData"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return {
+      chartData: {
+        nilaiAgama: 0,
+        motorikHalus: 0,
+        motorikKasar: 0,
+        bahasa: 0,
+        kognitif: 0,
+        sosialEmosi: 0,
+        seni: 0
+      }
+    }
+  },
+  methods:{
+    fetchChartData(){
+      //masukin satu2 ke chartData
+    }
+  },
+  created(){
+    this.fetchChartData()
+  }
 }
 </script>
 <style lang="scss" scoped>
+.chartContainer{
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
 .back_button{
   width: 32px !important;
   height:32px;
@@ -42,11 +69,14 @@ export default {
   padding: 1rem;
 }
 
-.chartContainer{
+.whiteBox{
   width: 70%;
   background: white;
   margin-left: auto;
-  padding: 1rem;
+  padding: 1.5rem;
   border-bottom-left-radius: 1rem;
+  display: flex;
+  flex-direction: column;
+  overflow: auto;
 }
 </style>
