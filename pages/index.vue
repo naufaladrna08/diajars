1d9c1fb63fec36cc0f0388b333bb1847f6983a36
naufaladrna08 <naufaladrn90@gmail.com>
2021-01-09 06:12:35 +0700

    ditambahkan openWindow utk redirect ke google api

--- pages/index.vue
+++ pages/index.vue
@@ -33,10 +33,51 @@ export default {
 	},
 	methods: {
 		signInGoogle() {
-			window.location.href = "http://localhost:8000/auth/google/callback"
+			// Why?
+			console.log("Tests")
+
+			const newWindow = openWindow('', 'message')
+			this.$axios.post('auth/google/callback')
+				.then(resp => {
+					newWindow.location.href = resp.data
+				})
+				.catch((error) => {
+          console.error(error);
+        });
+
 			state = 'pressed'
 		}
 	}
+}
+
+function openWindow(url, title, options = {}) {
+  if (typeof url === 'object') {
+    options = url
+    url = ''
+  }
+
+  options = { url, title, width: 600, height: 720, ...options }
+
+  const dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : window.screen.left
+  const dualScreenTop = window.screenTop !== undefined ? window.screenTop : window.screen.top
+  const width = window.innerWidth || document.documentElement.clientWidth || window.screen.width
+  const height = window.innerHeight || document.documentElement.clientHeight || window.screen.height
+
+  options.left = ((width / 2) - (options.width / 2)) + dualScreenLeft
+  options.top = ((height / 2) - (options.height / 2)) + dualScreenTop
+
+  const optionsStr = Object.keys(options).reduce((acc, key) => {
+    acc.push(`${key}=${options[key]}`)
+    return acc
+  }, []).join(',')
+
+  const newWindow = window.open(url, title, optionsStr)
+
+  if (window.focus) {
+    newWindow.focus()
+  }
+
+  return newWindow
 }
 </script>
 
