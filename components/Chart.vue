17b1385456626555ef9c7731f2e47b974d9692ee
Naufal Adriansyah <naufaladrn90@gmail.com>
2021-02-02 07:00:58 +0700

    chart

--- components/Chart.vue
+++ components/Chart.vue
@@ -5,32 +5,7 @@ export default {
   extends: Bar,
   mixins: [reactiveProp],
   mounted () {
-    this.renderChart({
-      labels: ['Nilai Agama', 'Motorik halus','Motorik kasar','Bahasa','Kognitif','Sosial emosi','Seni'],
-      datasets: [
-        {
-          label: 'Skor perkembangan anak',
-          backgroundColor: [
-                'rgba(255, 99, 132)',
-                'rgba(54, 162, 235)',
-                'rgba(255, 206, 86)',
-                'rgba(75, 192, 192)',
-                'rgba(153, 102, 255)',
-                'rgba(255, 159, 64)',
-                'rgb(70,189,135)'
-            ],
-          data: [this.chartData.nilaiAgama,
-                this.chartData.motorikHalus,
-                this.chartData.motorikKasar,
-                this.chartData.bahasa,
-                this.chartData.bahasa,
-                this.chartData.kognitif,
-                this.chartData.sosialEmosi,
-                this.chartData.seni
-                ]
-        },
-      ]
-    },
+    this.renderChart(this.chartData,
     {
       responsive: false,
       scales: {
