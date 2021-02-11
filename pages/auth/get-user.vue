f1e4611a096a27384d7eb9b09c9c440b78740cf6
Naufal Adriansyah <naufaladrn90@gmail.com>
2021-02-11 08:57:28 +0700

    tambah game

---
+++ pages/auth/get-user.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="container">
+		{{ id }}
+	</div>	
+</template>
+
+<script>
+export default {
+	data(){
+    return {
+      id: this.$auth.user.id
+    }
+  }
+}
+</script>
---
+++ pages/play/mengenal_huruf.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="root">
+    <div class="container">
+      <iframe src="http://localhost/games/mengenal_huruf/" scrollbar="no">  </iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+#root {
+  width: 100%;
+  height: 100%;
+  background: rgb(28, 15, 209);
+}
+.container {
+  width: 80%;
+  height: 100%;
+  margin: auto;
+  box-sizing: border-box;
+}
+
+iframe {
+  width: 100%;
+  height: 100%;
+  border: 0;
+  overflow: hidden;
+}
+</style>
