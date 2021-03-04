0cbe36a7baf914e148a3f9c56f0da46f28895c80
Naufal Adriansyah <naufaladrn90@gmail.com>
2021-03-04 23:35:17 +0700

    mencoba seluruh kemungkinan

--- pages/auth/social-callback.vue
+++ pages/auth/social-callback.vue
@@ -5,30 +5,57 @@
 </template>
 
 <script>
+// export default {
+// 	data() {
+// 		return {
+// 			token: this.$route.query.token ? this.$route.query.token : null
+// 		}
+// 	},
+// 	async mounted() {
+// 		await this.$auth.login({data: {
+// 			token: this.token
+// 		}}).catch((e) => {
+// 			console.log(e)	
+// 		})
+
+// 		console.log(this.$auth.user.jenisAkun)
+
+// 		switch (this.$auth.user.jenisAkun) {
+// 			case 'guru':
+// 				this.$router.push('/guru')
+// 				break
+// 			case 'murid':
+// 				this.$router.push('/murid')
+// 				break
+// 		}
+// 	},
+// 	auth: 'guest'
+// }
+
 export default {
-	data() {
-		return {
-			token: this.$route.query.token ? this.$route.query.token : null
-		}
-	},
-	async mounted() {
-		await this.$auth.login({data: {
-			token: this.token
-		}}).catch((e) => {
-			console.log(e)	
-		})
-
-		console.log(this.$auth.user.jenisAkun)
-
-		switch (this.$auth.user.jenisAkun) {
-			case 'guru':
-				this.$router.push('/guru')
-				break
-			case 'murid':
-				this.$router.push('/murid')
-				break
-		}
-	},
-	auth: 'guest'
+  middleware: ['guest'],
+  data() {
+    return {
+      token: this.$route.query.token ? this.$route.query.token : null
+    }
+  },
+  mounted() {
+    this.$auth.setToken('local', 'Bearer ' + this.token);
+    this.$auth.setStrategy('local');
+    this.$auth.fetchUser().then( () => {
+	    switch (this.$auth.user.jenisAkun) {
+				case 'guru':
+					this.$router.push('/guru')
+					break
+				case 'murid':
+					this.$router.push('/murid')
+					break
+			}
+    }).catch( (e) => {
+      this.$auth.logout();
+      console.log(e)	
+    });
+  },
+  auth: 'guest'  
 }
 </script>
